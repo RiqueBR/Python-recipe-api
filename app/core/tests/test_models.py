82bc5fb0a7e2a7c26f465d79958a8981aa7491f3
Henrique Batista <henrique.batista@ed.ac.uk>
2020-08-19 18:28:10 +0100

    lints the shit out of it

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core import models
 
 class ModelTests(TestCase):
-    
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'email@email.com'
